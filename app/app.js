a6fa7f4f74b11fb76d2f9f18eafeaf788779d09d
Vineet Upadhyay <vinnieupadhyay@gmail.com>
2017-06-10 22:18:46 +0530

    basic routing with express

--- app/app.js
+++ app/app.js
@@ -6,6 +6,13 @@ var dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
+    res.send(`
+        <h1>Welcome</h1>
+        <p>Meetups put together artists form all walks of life.</p>
+    `);
+});
+
+app.get('/speakers', function(req, res) {
     var info = '';
     dataFile.speakers.forEach(function(item) {
         info += `
@@ -20,6 +27,16 @@ app.get('/', function(req, res) {
         ${info}
     `);
 });
+
+app.get('/speakers/:speakerid', function(req, res) {
+    var speaker = dataFile.speakers[req.params.speakerid];
+    res.send(`
+        <h2>${speaker.title}</h2>
+        <h3>${speaker.name}</h3>
+        <p>${speaker.summary}</p>
+    `);
+});
+
 
 var server = app.listen(app.get('port'), function() {
   console.log('App running on port ' + app.get('port'));
